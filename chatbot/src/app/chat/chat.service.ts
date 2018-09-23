014669addad92b2fa342aa44c24284d256183422
Shaktia2z <shakti.kumar.delhi@gmail.com>
2018-09-23 13:17:25 +0530

    chatBot

---
+++ chatbot/src/app/chat/chat.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+import { ApiAiClient } from 'api-ai-javascript';
+
+////import { Observable } from 'rxjs/Observable';
+//import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+export class Message {
+  constructor(public content: string, public sentBy: string) {}
+}
+
+@Injectable()
+export class ChatService {
+
+  readonly token = environment.dialogflow.AngularBot;
+  readonly client = new ApiAiClient({ accessToken: this.token });
+
+  //conversation = new BehaviorSubject<Message[]>([]);
+
+  constructor() {}
+
+  talk() {
+    this.client.textRequest('Who are you!')
+      .then(res => console.log('Hello'));
+  }
+  // Sends and receives messages via DialogFlow
+  // converse(msg: string) {
+  //   const userMessage = new Message(msg, 'user');
+  //   this.update(userMessage);
+
+  //   return this.client.textRequest(msg)
+  //              .then(res => {
+  //                 const speech = res.result.fulfillment.speech;
+  //                 const botMessage = new Message(speech, 'bot');
+  //                 this.update(botMessage);
+  //              });
+  // }
+
+
+
+  // // Adds message to source
+  // update(msg: Message) {
+  //   this.conversation.next([msg]);
+  // }
+
+
+}
